0eb5b9002b3752298b08ece035afe956e0663a61
azifico <azifico@gmail.com>
2020-03-01 01:24:17 -0600

    foto

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import { saludar } from './js/componentes.js';
-import './styles.css';
 
-saludar('JONNY');
+import './styles.css';
+// import { Todo } from './classes/todo.class.js';
+// import { TodoList } from './classes/todo-list.class';
+import {Todo,TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+// const tarea = new Todo('aprender js');
+
+// todoList.nuevoTodo(tarea);
+
+console.log('todos',todoList.todos);
+
+//crearTodoHtml( tarea );
+
+todoList.todos.forEach( todo => crearTodoHtml( todo ) );
